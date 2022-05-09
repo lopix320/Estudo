01f9208a14385d97efc741867844dca759fcc97c
Gabriel Lopes <gabriel.lp2008@gmail.com>
2022-05-09 00:30:13 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# import pyautogui as bot
+
+# bot.PAUSE = 0.5
+# bot.press("win")
+# bot.write("Google Chrome")
+# bot.press("enter")
+# bot.write("Ryan Gosling")
+# bot.press("enter")
+                                                                                                        
+str = 'teste'
+print(str)
